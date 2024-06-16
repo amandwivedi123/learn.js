2bd22ed77c30bcf95064d8d79b1a1964c1ce2c1f
Aman Dwivedi <135406107+amandwivedi123@users.noreply.github.com>
2024-06-16 11:29:01 +0000

    added some string methods

---
+++ 01_basics/3.string.js
@@ -0,0 +1,65 @@
+// string is defined under "" or '';
+
+// let name = "ILove-JavaScript";
+// let anotherName ="           & i am trying to learn it         ";
+
+// // STRING METHODS
+
+// console.log(name.toUpperCase()); // to print the string on upperCase
+// console.log(name.length);   // To print the length of the string,
+// console.log(name.charAt(5)) // to indentify the charecter at particular index,
+// console.log(name.indexOf("o")); // to find the index of particular string value.
+
+// let newName = name.slice(1,4); // to print a  particular section of string
+// console.log(newName);
+
+// let newNameconCat = name.concat(anotherName);  // to add multiple  strings.
+// console.log(newNameconCat);
+
+// console.log(anotherName.trim()); // to remove  the unwanted whitespaces of string.
+
+// const url = "https://www.amandwivedi.%20/dwivedi.com";  // to replace new string with new one
+
+// console.log(url.replace ("%20" , "-"));
+
+// console.log(url.includes("aman"));
+
+// console.log(url.split("-"));
+// let compareStr ="I";  // To Compare the value of two strings
+// let compareStr2 = "J"
+// if("I" > "J"){
+//     console.log(`${compareStr} is greater than ${compareStr2}`);
+// } else if("J" > "I"){
+//     console.log(`${compareStr2} is greater than ${compareStr}`);
+// };
+
+// let strLength = "Aman \n Dwivedi"; // returns the length of the string
+// console.log(strLength)
+// console.log(strLength.length);
+
+// console.log(strLength[3]); // accessing the value of perticular string
+
+//  STRING METHODs 
+
+// let toUpper =" i love Javascript";  // print all the  charecters on uppercase
+// let newUpper = toUpper.toUpperCase();  // does not  change the original value but returns new value
+// console.log(toUpper);
+// console.log(newUpper)
+// const lower = "i Love programming";
+// let lowrOriginal = lower.toLowerCase();
+// console.log(lower)
+// console.log(lowrOriginal);
+
+// let trimStr = "            Hello i am aman             ";
+//  let originalVal = trimStr.trim();
+// console.log(trimStr);
+// console.log(originalVal);
+
+let str1 ="hello world"; // splits the charecters into array in separate charecters
+   console.log(str1.split(""))
+
+   console.log(str1.repeat(4))  // repeats the value of string again and again
+
+   console.log(str1.endsWith("d")) // ask if the last charecter  of the string ends with ??.... returns the value on true or false...
+
+
