f96fe5cb3838544dcdad09e625a0dcd7e178ed15
Aman Dwivedi <135406107+amandwivedi123@users.noreply.github.com>
2024-06-16 12:33:03 +0000

    added number methods

---
+++ 01_basics/4.numbers.js
@@ -0,0 +1,32 @@
+// ********* NUMBERS *********
+
+// let balance = 2353.12;
+
+// console.log(balance);
+
+//  let toCon = balance.toString(); // converted string into number provides new number
+//  console.log(toCon.length); 
+// console.log(typeof toCon); 
+
+// console.log(balance.toFixed(2)) // .toFixed works only with numbers.
+
+// console.log(balance.toPrecision(5))  // precise value 
+
+// let localVal = 100000000;
+// console.log(localVal.toLocaleString("en-IN"))
+
+
+// ********* Math ******
+
+// Absolute  value   converts (-) into plus
+
+let  num = -34.6;
+console.log(Math.abs(num))
+console.log(Math.round(num)) //  gives the round figure of value
+
+console.log("Minimum value on 2,3,5,4,6,7 is", Math.min(2,3,5,4,6,7)) // returns the minimum value of numbers
+
+console.log( "Maximum value of 4,5,6,8,9,34 is", Math.max(4,5,6,8,9,34));
+
+console.log(Math.random()*10+1) // to get the random values // provides the value between 0 to 1 ..ew have to multiply the values according to requirement.
+
