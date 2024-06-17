b4a30fb433debe5800223839063a370fda828409
Aman Dwivedi <135406107+amandwivedi123@users.noreply.github.com>
2024-06-17 08:30:30 +0000

    added dates

--- 01_basics/5.dates.js
+++ 01_basics/5.dates.js
@@ -1,4 +1,23 @@
 // DATES
 
-let date = Date();
-console.log(date);
+// let date = new Date();
+// console.log(date.toString());
+
+// console.log(date.toISOString())  
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toString());
+// console.log(date.toTimeString());
+// console.log(date.toUTCString());
+// console.log(date.getTimezoneOffset());
+// console.log(typeof date);
+
+const newDate = new Date(2024, 1, 12);
+console.log(newDate.toString());
+
+const myDate = new Date("01-12-2024");
+console.log(myDate.getTime());
+
+const myTimeStamp = Date.now();
+console.log(myTimeStamp);
