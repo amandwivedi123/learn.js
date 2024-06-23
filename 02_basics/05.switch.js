c997adc39714b8f51836a270aee40201390f61d0
Aman Dwivedi <135406107+amandwivedi123@users.noreply.github.com>
2024-06-23 07:52:36 +0000

    Switches

---
+++ 02_basics/05.switch.js
@@ -0,0 +1,68 @@
+  // *********** Switch *************
+  let month = 13;
+  switch (month) {
+   case 1 :
+   console.log("January");
+   break;
+ 
+   case 2 :
+     console.log("February");
+     break;
+ 
+   case 3 :
+     console.log("March");
+     break;
+ 
+   case 4 :
+     console.log("April");
+     break;
+ 
+   case 5 :
+     console.log("May");
+     break;
+ 
+   case 6 :
+     console.log("June");
+     break;
+ 
+   case 7 :
+     console.log("July");
+     break;
+ 
+   case 8 :
+     console.log("August");
+     break;
+ 
+   case 9 :
+     console.log("October");
+     break;
+     
+   case 10 :
+     console.log("November");
+     break;
+ 
+   case 11 :
+     console.log("December");
+     break;
+ 
+     default :
+     console.log("Undefined");
+  }
+ 
+ 
+  let mark =  32;
+ 
+  switch (mark){
+   case mark > 33:
+     console.log("Passed with grace");
+     
+ 
+     case mark <= 33 :
+       console.log("Failed");
+       break;
+ 
+     default :
+     console.log("Passed")
+     break;
+  }
+ 
