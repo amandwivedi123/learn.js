5ce6b78eb8e33ecb247fe4ee0a103dca7e63dae1
Aman Dwivedi <135406107+amandwivedi123@users.noreply.github.com>
2024-06-23 07:52:24 +0000

    Added operators

---
+++ 02_basics/04.operators.js
@@ -0,0 +1,64 @@
+// Operators 
+
+// 1. Arithmatic Operators 
+
+//  (++) Increment 
+// --  Decriment 
+// %  Remiander
+// * Multiply
+
+// 2. Assignment Operators  
+
+//Assignment	x = f()	
+// Addition assignment	x += f()	
+// Subtraction assignment	x -= f()	
+// Multiplication assignment	x *= f()	
+// Remainder assignment	x %= f()	
+// Exponentiation assignment	x **= f()
+// Left shift assignment	x <<= f()	
+// Right shift assignment	x >>= f()	
+// Unsigned right shift assignment	x >>>= f()	
+// Bitwise AND assignment	x &= f()	
+// Bitwise XOR assignment	x ^= f()	
+// Bitwise OR assignment	x |= f()	
+// Logical AND assignment	x &&= f()	)
+// Logical OR assignment	x ||= f()	
+// Nullish coalescing assignment	x ??= f()	
+
+
+// Comparison Operators
+
+// Equal (==)	Returns true if the operands are equal.	3 == var1
+
+// Not equal (!=)	Returns true if the operands are not equal.	
+
+// Strict equal (===)	Returns true if the operands are equal and of the same type. See also Object.is and sameness in JS.	
+// Strict not equal (!==)	Returns true if the operands are of the same type but not equal, or are of different type.	
+
+// Greater than (>)	Returns true if the left operand is greater than the right operand.	var2 > var1
+
+// Greater than or equal (>=)	Returns true if the left operand is greater than or equal to the right operand.
+
+// Less than (<)	Returns true if the left operand is less than the right operand.	
+
+// Less than or equal (<=)	Returns true if the left operand is less than or equal to the right operand.
+
+
+
+// Logical OPerators
+
+//Logical AND (&&)	...
+
+// Logical OR (||)...
+
+// Logical NOT (!)...	
+
+
+// Bitwise Operators..
+
+// Bitwise AND	a & b	
+// Bitwise OR	a | b	
+// Bitwise XOR	a ^ b	
+// Bitwise NOT	~ a	Inverts the bits of its operand.
+// Left shift	a << b	.
+// Zero-fill right shift	a >>> b	.
